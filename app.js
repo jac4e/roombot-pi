3d1fad77eb21035f1acb0ba07025e148a3d30718
JacquesFourie <jacludvic4e@gmail.com>
2020-03-02 03:08:39 -0700

    pi specific additions

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const serverModule = require('./modules/serverModule');
-//const boardModule = require('./modules/boardModule');
+const boardModule = require('./modules/boardModule');
 const visionModule = require('./modules/visionModule');
 
 const server = serverModule(8080);
-//const rpi = boardModule();
+const rpi = boardModule();
 server.init();
 server.createSocket('moveForward', () => {
-    //  rpi.sendCommand('moveForward',10);
+    rpi.sendCommand('moveForward',10);
     console.log('moveForward');
 });
 server.createSocket('moveBackward', () => {
-    //  rpi.sendCommand('moveForward',10);
+    rpi.sendCommand('moveBackward',10);
     console.log('moveBackward');
 });
 server.createSocket('turnLeft', () => {
-    //  rpi.sendCommand('moveForward',10);
+    rpi.sendCommand('turnLeft',10);
     console.log('turnLeft');
 });
 server.createSocket('turnRight', () => {
-    //  rpi.sendCommand('moveForward',10);
+    rpi.sendCommand('turnRight',10);
     console.log('turnRight');
 });
