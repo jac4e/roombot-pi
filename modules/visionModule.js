b720a437b1253c8e73329af15339665a1c76e7ee
JacquesFourie <jacludvic4e@gmail.com>
2020-03-03 12:35:23 -0700

    changed img processing to canny detection

--- modules/visionModule.js
+++ modules/visionModule.js
@@ -13,19 +13,22 @@ class visionModule {
         return this.process(img);
     }
     process(img) {
-        let frame = img.resize(640,360);
-        const low1 = new cv.Vec3(0, 0, 120);
-        const up1 = new cv.Vec3(180, 20, 255);
-        const low2 = new cv.Vec3(140, 46, 6);
-        const up2 = new cv.Vec3(170, 255, 255);
+        let frame = img.resize(360,640);
+        // const low1 = new cv.Vec3(90, 19, 223);
+        // const up1 = new cv.Vec3(108, 88, 253);
+        // const low2 = new cv.Vec3(95, 12, 120);
+        // const up2 = new cv.Vec3(115, 30, 180);
 
-        frame = frame.cvtColor(cv.COLOR_BGR2HSV);
-        const mask1 = frame.inRange(low1, up1);
-        //const mask2 = frame.inRange(low2,up2);
-        frame = mask1 //.or(mask2);
-        //frame = frame.blur(new cv.Size(10, 10));
-        //frame = frame.threshold(200, 255, cv.THRESH_BINARY);
-        return [cv.imencode('.jpg', frame).toString('base64'), cv.imencode('.jpg', img).toString('base64')];
+        // frame = frame.blur(new cv.Size(10, 10));
+        // frame = frame.cvtColor(cv.COLOR_BGR2HSV);
+        // const mask1 = frame.inRange(low1, up1);
+        // const mask2 = frame.inRange(low2,up2);
+        // frame = mask1.or(mask2);
+        // frame = frame.threshold(, 255, cv.THRESH_BINARY);
+        frame = frame.cvtColor(cv.COLOR_BGR2GRAY);
+        let frameBlur = frame.gaussianBlur(new cv.Size(9,9),3);
+        frame = frameBlur.canny(0,120);
+        return [cv.imencode('.jpg', frame).toString('base64'), cv.imencode('.jpg', frameBlur).toString('base64')];
     }
     getBounderies() {
         // How image processing should hopefully work
@@ -39,3 +42,18 @@ function create() {
 }
 
 module.exports = create
+// RGB
+// 210,253,253
+// 175,212,251
+// 229,244,247
+// 146,177,223
+// HSV(need to h / 2 s and v /100 * 255)
+// 90,43,99.2
+// 105,77,98.4
+// 95,19,96.9
+// 108,88,87.5
+
+
+
+
+
