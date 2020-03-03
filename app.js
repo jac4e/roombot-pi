404a5b31bf02f10ea5cec1bcb970fa84bd6fb698
JacquesFourie <jacludvic4e@gmail.com>
2020-03-03 12:35:23 -0700

    changed img processing to canny detection

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 const serverModule = require('./modules/serverModule');
-const boardModule = require('./modules/boardModule');
+// const boardModule = require('./modules/boardModule');
 const visionModule = require('./modules/visionModule');
 
 const server = serverModule();
-const rpi = boardModule();
+// const rpi = boardModule();
 server.init();
 server.createSocket('moveForward', () => {
-    rpi.moveCmd('moveForward');
+    // rpi.moveCmd('moveForward');
     console.log('moveForward');
 });
 server.createSocket('moveBackward', () => {
-    rpi.moveCmd('moveBackward');
+    // rpi.moveCmd('moveBackward');
     console.log('moveBackward');
 });
 server.createSocket('turnLeft', () => {
-    rpi.moveCmd('turnLeft');
+    // rpi.moveCmd('turnLeft');
     console.log('turnLeft');
 });
 server.createSocket('turnRight', () => {
-    rpi.moveCmd('turnRight');
+    // rpi.moveCmd('turnRight');
     console.log('turnRight');
 });
 server.createSocket('stop', () => {
-    rpi.moveCmd('stop');
+    // rpi.moveCmd('stop');
     console.log('stop');
 });
 
 const test = visionModule();
 const fps = 30;
-server.initS30tream(2);
+server.initStream(4);
 setInterval(() => {
-    server.stream(test.camView());
+    server.stream(test.mapView().concat(test.camView()));
 }, 1000 / fps);
