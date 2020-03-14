0b848a712e12763eba49ef589a4f85664e8e95e2
JacquesFourie <jacludvic4e@gmail.com>
2020-03-14 07:16:51 -0600

    updated kalman filter

--- sensorTest.js
+++ sensorTest.js
@@ -2,10 +2,9 @@ const boardModule = require("./modules/boardModule.js");
 const trackingModule  = require("./modules/trackingModule.js")
 const { Matrix } = require('ml-matrix');
 const fs = require('fs');
-const pi = boardModule();
+const pi = boardModule(interval);
 
 const dt = 0.1;
-const kalman = trackingModule(dt);
 
 const A = new Matrix([
     [1, 0, dt, 0],
@@ -23,7 +22,10 @@ const C = new Matrix([
     [1,0,0,0],
     [0,1,0,0]
 ]);
+const L = B.mmul(B.transpose()).mul(0.0312 ^ 2);
+const kalman = trackingModule(dt,L);
 
+let data = [];
 function interval() {
     pi.initSensors();
     setInterval(() => {
@@ -31,7 +33,7 @@ function interval() {
             pi.readAccel("Y"),(aY) => { 
                 data.push([aX,aY]);
                 const input = new Matrix([[aX],[aY]]);
-                const m = new Matrix( [ [aX * ( (dt^2)/2 )], [aY * ( (dt^2)/2 )] , [aX * (dt)] , [aY * (dt)] ] );
+                const m = new Matrix( [ [aX * ( (dt^2)/2 )], [aY * ( (dt^2)/2 )] ] );
                 const m2 = Matrix.zeroes(1,4)
                 kalman.update(input,m,A,B,C);
             }
